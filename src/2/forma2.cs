226263e8553a223d0b6315964685790876bd7e31
Bin.Alex <86982162+BinAlexme@users.noreply.github.com>
2025-04-03 12:39:42 +0300

    Update 2
    видоизменил и получил конечный результат. процесс с комментированием.

--- src/2/forma2.cs
+++ src/2/forma2.cs
@@ -11,31 +11,31 @@ namespace paint
             InitializeComponent();
         }
 
-        private void Form2_MouseDown(object sender, MouseEventArgs e)
+        private void Form2_MouseDown(object sender, MouseEventArgs e) // вызываем запрос взамодействия мыши
         {
+
             g = CreateGraphics();
-            mousePresed = true;
+            mousePresed = true; //мыши кнопка зажата 
             x1 = x2 = e.X;
             y1 = y2 = e.Y;
-            rectangleDash = rectangleLine = Rectangle.FromLTRB(x1, y1, x2, y2);
+            rectangleDash = rectangleLine = Rectangle.FromLTRB(x1, y1, x2, y2); // прямоуголькин штриховка объединяем с прямоугольником линий
         }
 
-        private void Form2_MouseMove(object sender, MouseEventArgs e)
+        private void Form2_MouseMove(object sender, MouseEventArgs e) // вызываем запрос перемещения мыши
         {
             if (mousePresed)
             {
+                // даем описание
                 Pen pen = new Pen(Color.Black, 3);
                 pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
                 Pen eraser = new Pen(Color.White, 3);
 
-                g.DrawRectangle(eraser, rectangleDash);
+                g.DrawRectangle(eraser, rectangleDash); // рисуем ластик и прямоугольник
 
                 x2 = e.X;
                 y2 = e.Y;
 
-                // упорядочивание координат
-                // таким образом, чтобы в первой паре аргументов
-                // передавались всегда меньшие значения координа
+                // сортировка координат
                 if ((x1 <= e.X) && (y1 >= e.Y))
                 {
                     rectangleLine = Rectangle.FromLTRB(x1, y2, x2, y1);
@@ -53,20 +53,37 @@ namespace paint
                     rectangleLine = Rectangle.FromLTRB(x1, y1, x2, y2);
                 }
 
+                // рисуем прямоугольнику линии и прочерки
                 g.DrawRectangle(pen, rectangleLine);
                 rectangleDash = rectangleLine;
             }
         }
 
+        private void Form2_MouseUp(object sender, MouseEventArgs e) // вызываем запрос ведение мыши
+        {
+            if (mousePresed)
+            {
+                Pen pen = new Pen(Color.Black, 3);
 
+                g.DrawRectangle(pen, rectangleLine);
+
+                mousePresed = false;
+            }
+        }
+
+
+        // обозначение всех точек
         int x1;
         int x2;
         int y1;
         int y2;
 
+        Rectangle rectangleDash; // прямогульник прочерк
+        Rectangle rectangleLine; // прямогульник линия
+
         Graphics g;
 
-        bool mousePresed = false;
+        bool mousePresed = false; // проверка нажата ли кнопка мыши (состояние мыши)
 
         private void Form2_Load(object sender, EventArgs e)
         {
